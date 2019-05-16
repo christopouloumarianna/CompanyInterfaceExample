2816c595b2aa8a58a1912ca5a2a0db7614ea4805
christopouloumarianna <50484642+christopouloumarianna@users.noreply.github.com>
2019-05-16 11:45:06 +0300

    Initial commit

---
+++ C.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyInterfaceExample
+{
+    public interface IEmployee
+    {
+        double GetSalary();
+        bool IsUpdated();
+
+    }
+    public interface IRemote
+    {
+        double GetTotalHours();
+    }
+    public interface ILocal
+    {
+        int GetAbsenceDays();
+    }
+    public class SalesPerson : IRemote, IEmployee
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public double GetSalary()
+        {
+            return Salary;
+        }
+
+        public double GetTotalHours()
+        {
+            return 80.0;
+        }
+
+
+
+
+        public SalesPerson(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public bool IsUpdated()
+        {
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CompanyInterfaceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new SalesPerson(45, "Dimitris", 896.0);
+            double salary = person1.GetSalary();
+
+            Console.WriteLine(person1.GetTotalHours());
+            Console.WriteLine(person1.GetSalary());
+            if (person1.IsUpdated())
+            {
+                Console.WriteLine("Updated");
+            }
+            else
+            {
+                Console.WriteLine("Not upadated");
+            }
+        }
+    }
+}
